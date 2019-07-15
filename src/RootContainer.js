c31a1746106f5adac1ca34d11c271047a293d5cf
akshat <akshatbhargavadeepa@gmail.com>
2019-07-15 11:54:03 +0530

    updated demo geneId for testing and integrated query

--- src/RootContainer.js
+++ src/RootContainer.js
@@ -1,6 +1,23 @@
 import React from 'react';
+import queryData from './queryData';
 
 class RootContainer extends React.Component {
+	componentDidMount() {
+		const {
+			entity: { value },
+			serviceUrl
+		} = this.props;
+		queryData(value, serviceUrl)
+			.then(res => {
+				const { atlasExpression } = res;
+				this.setState({
+					chartData: atlasExpression,
+					loading: false
+				});
+			})
+			.catch(error => this.setState({ error, loading: false }));
+	}
+
 	render() {
 		return (
 			<div className="rootContainer">
